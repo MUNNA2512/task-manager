4d08c1a5ddbef7c9da24b2c37b8f323d93833ae3
munna kumar <m6997500@gmail.com>
2025-04-19 18:19:47 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_python_project',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'my_python_project=my_python_project.main:main',
+        ],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A simple Python project with a greeting function.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/my_python_project',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+) 
